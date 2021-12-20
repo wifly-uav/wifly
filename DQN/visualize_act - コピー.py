704c31c087de145c15cf6cd2d7c4014d07c093bb
Ok-Rk <riku3141@gmail.com>
2021-12-20 20:18:29 +0900

    result

--- DQN/visualize_act - コピー.py
+++ DQN/visualize_act - コピー.py
@@ -36,28 +36,18 @@ class visual_act:
             sum += i
             time.append(sum/1000)
         
-        fig = plt.figure()
-        ax1 = fig.add_subplot(111)
-        ln1 = ax1.plot(time, angle, color="red", label='angle')        
+        
+        plt.plot(time,angle)
+        plt.ylim(-90,90)
+        plt.plot([0, time[-1]],[10, 10], "red", linestyle='dashed')
+        plt.plot([0, time[-1]],[-10, -10], "red", linestyle='dashed')
+        plt.plot([0, time[-1]],[0, 0], "black")
+        plt.title("ANGLE",fontsize=25)
+        plt.savefig(self.folder + '/angle.png')
+        plt.show()    
         #_ = ax1.plot([0, time[-1]],[10, 10], "orange", linestyle='dashed')
         #_ = ax1.plot([0, time[-1]],[-10, -10], "orange", linestyle='dashed')
-
-        ax2 = ax1.twinx()
-        ln2 = ax2.plot(time, q_max, color="green", linestyle='dashed',linewidth = 0, marker='.', label='Q_max')
         #ln3 = ax2.plot(time, act, color="blue", linestyle='dashdot', linewidth = 0.3, label='Action')
-        
-        h1, l1 = ax1.get_legend_handles_labels()
-        h2, l2 = ax2.get_legend_handles_labels()
-        ax1.legend(h1+h2, l1+l2, loc='lower right')
-
-        ax1.set_ylim([-180, 180])
-        #ax1.set_xlim([50, 60])
-        ax2.set_ylim([0,6])
-
-        if self.save:
-            plt.savefig(self.folder + '/act.png')
-        if self.flag:
-            plt.show()
 
 
 if __name__ == "__main__":
