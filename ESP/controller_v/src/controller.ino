cee07db13b32f3bd9a9fc0a5d4a8c97ae764a744
NomizuAkira <nomizu89@fuji.waseda.jp>
2022-10-26 16:33:39 +0900

    a

--- ESP/controller_v/src/controller.ino
+++ ESP/controller_v/src/controller.ino
@@ -2,22 +2,42 @@
 #include <WiFi.h>
 
 #include <SPI.h>
+#include <PID_v1.h>
 //#define DEBUG
+
+typedef struct param_{
+  double kp = 0;
+  double ki = 0;
+  double kd = 0;
+  double Setpoint;
+  double Input;
+  double Output;
+  double max;
+  double min = 0;
+  float dt = 40;
+}param;
+
+param servo;
+param motor;
 
 unsigned long lastTime = 0;  
 unsigned long timerDelay = 60;  // send readings timer
 size_t data_pc_;
 uint8_t data_pc[5] = {0};
 int re_data[5] = {0};
-double re_data_angle[4] = {0};   
+double re_data_angle[4] = {0}; 
+//各ボタンのピン番号
+int stick_lr,stick_ud,slider_l,slider_r,switch_1,switch_2,volume;
+double ysqr,t0,t1,t2,t3,t4 = 0;
+int eular[3] = {0};
 
 // REPLACE WITH RECEIVER MAC Address
 //機体側のマイコンの番号にあったアドレスのみコメントアウトを外す。
 //uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0xA1, 0x60}; //1
 //uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0xA1, 0x3D}; //2
-//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0x95, 0xE4}; //3
+uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0xA2, 0x22}; //3
 //uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0xA1, 0xAC}; //4
-uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0x95, 0x98}; //5
+//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0x95, 0x98}; //5
 //uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0xA2, 0xBF};   //6
 
 esp_now_peer_info_t peerInfo;
@@ -25,134 +45,31 @@ esp_now_peer_info_t peerInfo;
 const int controller_num = 2; //1:A 2:B
 const float servo_sensitivity = 0.2; //0.0(min)~1.0(max)
 
-//pin
-//各ボタンのピン番号
-int stick_lr;
-int stick_ud;
-int slider_l;
-int slider_r;
-int switch_1;
-int switch_2;
-int volume;
+void qu2eu(int eular[3], double quotanion[4]);
+void onReceive(const uint8_t* mac_addr, const uint8_t* data, int data_len);
+void OnDataSent(const uint8_t* mac_addr, esp_now_send_status_t status);
+void recieve_pc();
 
-double ysqr = 0;
-double t0,t1,t2,t3,t4 = 0;
-int eular[3] = {0};
-
-//quotanion（四元数）をEuler角に変換する関数
-void qu2eu(int eular[3], double quotanion[4]){
-  double w = quotanion[0];
-  double x = quotanion[1];
-  double y = quotanion[2];
-  double z = quotanion[3];
-
-  double sz = -(2 * x * y - 2 * z * w);  
-  double a = asin(sz);
-  int flag = cos(a) != 0;
-  eular[0] = flag ? atan2(2 * y * z + 2 * x * w, 2 * w * w + 2 * y * y - 1)/PI*180 : atan2(-(2 * y * z - 2 * x * w), 2 * w * w + 2 * z * z - 1)/PI*180;
-  eular[1] = flag ? atan2(2 * x * z + 2 * y * w, 2 * w * w + 2 * x * x - 1)/PI*180 : 0;
-  eular[2] = a/PI*180;
-
-  /*
-  ysqr = y * y;
-
-  // roll (x-axis rotation)
-  t0 = +2.0 * (w * x + y * z);
-  t1 = +1.0 - 2.0 * (x * x + ysqr);
-  eular[0] = atan2(t0, t1)/PI*180;
-
-  // pitch (y-axis rotation)
-  t2 = +2.0 * (w * y - z * x);
-  t2 = t2 > 1.0 ? 1.0 : t2;
-  t2 = t2 < -1.0 ? -1.0 : t2;
-  eular[1] = asin(t2)/PI*180;
-
-  // yaw (z-axis rotation)
-  t3 = +2.0 * (w * z + x * y);
-  t4 = +1.0 - 2.0 * (ysqr + z * z);  
-  eular[2] = atan2(t3, t4)/PI*180;
-  */
-}
-
-//受信時コールバック関数（データを受信すると実行される）
-//引数:送信元macアドレス情報,受信データ,受信データ長
-void onReceive(const uint8_t* mac_addr, const uint8_t* data, int data_len) {
-    char macStr[18];
-
-    //データの送信元のmacアドレスを表示する
-    //%02Xは2桁以上の16進数で表示することを指定（桁が足りない場合、上位の桁が0埋めされる。）
-    snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
-        mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-    //Serial.println();
-    //Serial.printf("ESP32\n");
-    //Serial.printf("Last Packet Recv from: %s\n", macStr);
-    //Serial.printf("Last Packet Recv Data(%d): ", data_len);
-    //Serial.println();
-    
-    for (int i = 0; i < data_len; i++) {
-        if(i > 4){
-          //受信データの5番目以降（quotanion）は、処理してre_data_angleに格納していく。
-          re_data_angle[i-5] = data[i]*0.01-1;
-        }else if(i < 2){
-          //受信データの先頭2つ（羽ばたき出力）は、0~255を反転してre_dataに格納
-          re_data[i] = 255 - data[i];
-        }
-        else{
-          //残りはそのままre_dataに格納
-          re_data[i] = data[i];
-        }
-    }
-
-    //カンマ区切りでre_dataの中身を表示
-    for (int i = 0; i < 5; i++) {
-      Serial.print(re_data[i]);
-      if(i != 4){
-        Serial.print(",");
-      }
-    }
-
-    //受信したquotanionをEuler角に変換
-    qu2eu(eular,re_data_angle);
-
-    Serial.print(",");
-
-    //Euler角を表示（角度の順番を要確認!）
-    for (int i = 0; i < 3; i++) {
-      Serial.print(eular[i]);
-      if(i != 2){
-        Serial.print(",");
-      }
-    }
-    Serial.println();
-}
-
-//送信時コールバック関数
-//送信が成功したかどうかを表示する。
-void OnDataSent(const uint8_t* mac_addr, esp_now_send_status_t status) {
-  Serial.println();
-  Serial.printf("ESP32\n");
-  Serial.print("Last Packet Send Status: ");
-  if (status == 0){
-    Serial.println("Delivery success");
-  }
-  else{
-    Serial.println("Delivery fail");
-  }
-}
-
-//PCからの情報を受信する関数
-void recieve_pc(){
-  if(Serial.available()){                               //シリアルポートにデータが来ている場合
-    if((int)Serial.read() == 255){                      //データの先頭が255である場合
-      data_pc_ = Serial.readBytesUntil('\n',data_pc,5); //5つのデータを読み取り、data_pc_に格納
-    }
-  }
-}
+PID pitch_PID(&servo.Input, &servo.Output, &servo.Setpoint, servo.kp, servo.ki, servo.kd, DIRECT);
 
 void setup() {
+  servo.kp = 1;
+  servo.ki = 0;
+  servo.kd = 0;
+  servo.max = 45;
+  servo.min = -45;
+  servo.dt = 40;
+
+  pitch_PID.SetOutputLimits(servo.max,servo.min);
+  pitch_PID.SetSampleTime(servo.dt);
+  pitch_PID.SetMode(AUTOMATIC);
+  pitch_PID.kp = servo.kp;
+  pitch_PID.ki = servo.ki;
+  pitch_PID.kd = servo.kd;
 
   //各コントローラごとのピン番号
   switch(controller_num){
+    
     case 1:
       stick_lr = 34;
       stick_ud = 35;
@@ -216,7 +133,6 @@ int sli_R;
 int vol;
 
 void loop() {
-  
   //トグルスイッチの入力値読み取り
   int btn_L = digitalRead(switch_1);
   int btn_R = digitalRead(switch_2);
@@ -224,15 +140,29 @@ void loop() {
   if ((millis() - lastTime) > timerDelay) { //受信間隔がtimerDelay以内であれば、
     uint8_t data[5];
 
+
+    servo.Setpoint = 0;
+    servo.Input = eular[1];
+    pitch_PID.Compute();
     //PCモード
     if(btn_L == 1){
-      recieve_pc();                         //PCからデータ受信
-      for(int i=0;i<2;++i){
-        data[i] = 254 - data_pc[i];         //受信データの先頭2つ（羽ばたき出力）は反転
-      }
-      for(int i=2;i<5;++i){
-        data[i] = data_pc[i];
-      }
+          left_LR = map(analogRead(stick_lr),0,4096,0,255); //揚力差
+          left_UD = map(analogRead(stick_ud),0,4096,0,255); //割り当てなし
+          sli_L = map(analogRead(slider_l),0,4096,90.0*(1.0-servo_sensitivity),90.0+90.0*servo_sensitivity);   //尾翼サーボ
+          sli_R = map(analogRead(slider_r),0,4096,0,255);   //羽ばたき出力
+            //スライドボリューム右（羽ばたき出力）最小
+          if(sli_R>240){sli_R = 255;}
+          //ジョイスティック左右中央
+          if(left_LR<152 && left_LR>102){left_LR = 127;}
+          //ジョイスティック上下中央
+          if(left_UD<152 && left_UD>102){left_UD = 127;}
+          int left_pwm = max(0,sli_R - max(0, (left_LR - 127)/4 ));  //左（要確認）
+          int right_pwm = max(0,sli_R - max(0, (127 - left_LR)/4)); //右（要確認）
+          
+          data[0] = left_pwm;          
+          data[1] = right_pwm;
+          data[2] = max(min(45,(int)servo.Output),-45)+45;
+          data[3] = -1*max(min(45,(int)servo.Output),-45)+45;
     
     //コントローラモード
     }else{  
@@ -253,7 +183,7 @@ void loop() {
           data[1] = sli_R;    //右羽ばたき出力
           data[2] = left_UD;  //サーボ1角（元は尾翼）
           data[3] = 180 - left_UD;  //サーボ2角（元はcog）
-          data[4] = btn_R;
+          //data[4] = btn_R;
           if(btn_R == 1){
             data[2] = 0;
             data[3] = 0;
@@ -291,6 +221,11 @@ void loop() {
           data[1] = right_pwm;
           data[2] = servo_angle;
           data[3] = cog_angle;
+          
+          if(btn_R == 1){
+            data[2] = 0;
+            data[3] = 0;
+          }
 
           break;
       }
@@ -336,9 +271,117 @@ void loop() {
 
     //delay(10);
 
+    Serial.print(servo.Input);
+    Serial.print(",");
+    Serial.println(servo.Output);
     // Send message via ESP-NOW
     esp_now_send(castAddress, (uint8_t *) &data, sizeof(data));
     //esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
     lastTime = millis();
+  }
+}
+
+//quotanion（四元数）をEuler角に変換する関数
+void qu2eu(int eular[3], double quotanion[4]){
+  double w = quotanion[0];
+  double x = quotanion[1];
+  double y = quotanion[2];
+  double z = quotanion[3];
+
+  double sz = -(2 * x * y - 2 * z * w);  
+  double a = asin(sz);
+  int flag = cos(a) != 0;
+  eular[0] = flag ? atan2(2 * y * z + 2 * x * w, 2 * w * w + 2 * y * y - 1)/PI*180 : atan2(-(2 * y * z - 2 * x * w), 2 * w * w + 2 * z * z - 1)/PI*180;
+  eular[1] = flag ? atan2(2 * x * z + 2 * y * w, 2 * w * w + 2 * x * x - 1)/PI*180 : 0;
+  eular[2] = a/PI*180;
+
+  /*
+  ysqr = y * y;
+
+  // roll (x-axis rotation)
+  t0 = +2.0 * (w * x + y * z);
+  t1 = +1.0 - 2.0 * (x * x + ysqr);
+  eular[0] = atan2(t0, t1)/PI*180;
+
+  // pitch (y-axis rotation)
+  t2 = +2.0 * (w * y - z * x);
+  t2 = t2 > 1.0 ? 1.0 : t2;
+  t2 = t2 < -1.0 ? -1.0 : t2;
+  eular[1] = asin(t2)/PI*180;
+
+  // yaw (z-axis rotation)
+  t3 = +2.0 * (w * z + x * y);
+  t4 = +1.0 - 2.0 * (ysqr + z * z);  
+  eular[2] = atan2(t3, t4)/PI*180;
+  */
+}
+
+//受信時コールバック関数（データを受信すると実行される）
+//引数:送信元macアドレス情報,受信データ,受信データ長
+void onReceive(const uint8_t* mac_addr, const uint8_t* data, int data_len) {
+    char macStr[18];
+
+    //データの送信元のmacアドレスを表示する
+    //%02Xは2桁以上の16進数で表示することを指定（桁が足りない場合、上位の桁が0埋めされる。）
+    snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
+        mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+    //Serial.println();
+    //Serial.printf("ESP32\n");
+    //Serial.printf("Last Packet Recv from: %s\n", macStr);
+    //Serial.printf("Last Packet Recv Data(%d): ", data_len);
+    //Serial.println();
+    for (int i = 0; i < data_len; i++) {
+        if(i > 4){
+          //受信データの5番目以降（quotanion）は、処理してre_data_angleに格納していく。
+          re_data_angle[i-5] = data[i]*0.01-1;
+        }else if(i < 2){
+          //受信データの先頭2つ（羽ばたき出力）は、0~255を反転してre_dataに格納
+          re_data[i] = 255 - data[i];
+        }
+        else{
+          //残りはそのままre_dataに格納
+          re_data[i] = data[i];
+        }
+    }
+    //カンマ区切りでre_dataの中身を表示
+    for (int i = 0; i < 5; i++) {
+      //Serial.print(re_data[i]);
+      if(i != 4){
+        //Serial.print(",");
+      }
+    }
+    //受信したquotanionをEuler角に変換
+    qu2eu(eular,re_data_angle);
+    //Serial.print(",");
+    //Euler角を表示（角度の順番を要確認!）
+    for (int i = 0; i < 3; i++) {
+      //Serial.print(eular[i]);
+      if(i != 2){
+        //Serial.print(",");
+      }
+    }
+    //Serial.println();
+}
+
+//送信時コールバック関数
+//送信が成功したかどうかを表示する。
+void OnDataSent(const uint8_t* mac_addr, esp_now_send_status_t status) {
+  Serial.println();
+  Serial.printf("ESP32\n");
+  Serial.print("Last Packet Send Status: ");
+  if (status == 0){
+    Serial.println("Delivery success");
+  }
+  else{
+    Serial.println("Delivery fail");
+  }
+}
+
+//PCからの情報を受信する関数
+void recieve_pc(){
+  if(Serial.available()){                               //シリアルポートにデータが来ている場合
+    if((int)Serial.read() == 255){                      //データの先頭が255である場合
+      data_pc_ = Serial.readBytesUntil('\n',data_pc,5); //5つのデータを読み取り、data_pc_に格納
+    }
   }
 }
