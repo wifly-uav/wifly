17825245768166438e323b21b38420495457fdda
Ok-Rk <riku3141@gmail.com>
2022-12-02 23:34:17 +0900

    a

---
+++ DQN2022/visualize_angle_act.py
@@ -0,0 +1,100 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import os
+import statistics
+from PIL import Image, ImageDraw, ImageFont
+import math
+
+class visual_act:
+    def __init__(self, flag=False, folder='log', action=6):
+        self.flag = flag
+        self.folder = folder
+        
+        self.im = Image.new("RGB", (10240,5400), (128,128,128))
+        self.draw = ImageDraw.Draw(self.im)
+
+        self.max_size = 5000
+        self.minimum_size = 100
+        self.min_x = 40
+        self.min_y = 100
+        self.center_x = self.max_size/2+self.min_x
+        self.center_y = self.max_size/2+self.min_y
+        self.action_num = action
+
+        self.min_x2 = 5200
+        self.min_y2 = 100
+        self.center_x2 = self.max_size/2+self.min_x2
+        self.center_y2 = self.max_size/2+self.min_y2
+
+        self.point_size = 30
+   
+        self.size = (self.max_size-self.minimum_size)/(2*self.action_num)
+        self.size2 = (self.max_size-self.minimum_size)/(2*3)
+
+    def draw_base(self):
+        for i in range(self.action_num):
+            self.draw.arc((self.min_x+self.size*i, self.min_y+self.size*i, self.min_x+self.max_size-self.size*i, self.min_y+self.max_size-self.size*i), start=0, end=360, fill=(255, 255, 0),width=10)
+        self.draw.ellipse((self.center_x-self.point_size, self.center_y-self.point_size, self.center_x+self.point_size, self.center_y+self.point_size), fill=(255, 0, 0), outline=(0, 0, 0))
+        self.draw.line((self.center_x, self.min_y, self.center_x, self.min_y+self.max_size), fill=(255, 255, 0), width=10)
+        self.draw.line((self.min_x, self.center_y, self.min_x+self.max_size, self.center_y), fill=(255, 255, 0), width=10)
+        #self.im.show()
+
+    def draw_base2(self):
+        for i in range(3):
+            self.draw.arc((self.min_x2+self.size2*i, self.min_y2+self.size2*i, self.min_x2+self.max_size-self.size2*i, self.min_y2+self.max_size-self.size2*i), start=0, end=360, fill=(255, 255, 0),width=10)
+        self.draw.ellipse((self.center_x2-self.point_size, self.center_y2-self.point_size, self.center_x2+self.point_size, self.center_y2+self.point_size), fill=(255, 0, 0), outline=(0, 0, 0))
+        self.draw.line((self.center_x2, self.min_y2, self.center_x2, self.min_y2+self.max_size), fill=(255, 255, 0), width=10)
+        self.draw.line((self.min_x2, self.center_y2, self.min_x2+self.max_size, self.center_y2), fill=(255, 255, 0), width=10)
+        #self.im.show()
+    
+    def angleact2pos(self, angle, act):
+        pos_x = self.center_x + self.size*(act+1)*math.cos(math.radians(angle-90))
+        pos_y = self.center_y + self.size*(act+1)*math.sin(math.radians(angle-90))
+        return 2*self.center_x-pos_x, pos_y
+
+    def load(self):
+        self.q = np.loadtxt(self.folder + '/debug_q.csv', delimiter=',')
+        self.act = np.loadtxt(self.folder + '/debug_act.csv', delimiter=',')
+        self.state = np.loadtxt(self.folder + '/log2.csv', delimiter=',')
+        self.pow = np.loadtxt(self.folder + '/log.csv', delimiter=',' ,usecols=[-4,-3])
+
+    def recog(self):
+        self.q_max = []
+        self.time = []
+        self.action = []
+        
+        for i in self.q:
+            self.q_max.append(np.argmax(i)+1)
+
+        self.angle = self.state[:, 0]
+        self.time_delta = self.state[:, 2]
+        self.action = self.act[:, 0]
+        self.pow_left = self.pow[:, 0]
+        self.pow_rigiht = self.pow[:, 1]
+        sum = 0
+        for i in self.time_delta:
+            sum += i
+            self.time.append(sum/1000)
+
+    def visualize(self):
+        num = len(self.angle)
+        for i in range(num):
+            x,y = self.angleact2pos(self.angle[i],self.action[i])
+            #print(str(x)+","+str(y))
+            self.draw.ellipse((x-self.point_size, y-self.point_size, x+self.point_size, y+self.point_size), fill=(255-int(255/num*i), 0, int(255/num*i)), outline=(0, 0, 0))
+        self.im.show()
+
+    def save(self):   
+        self.im.save(self.folder + '/angle_act.jpg', quality=10)
+
+
+if __name__ == "__main__":
+    path = os.path.dirname(__file__)
+
+    ac = visual_act(flag=1,folder=path + '/result/1117_pre_5')
+    ac.load()
+    ac.recog()
+    ac.draw_base()
+    ac.draw_base2()
+    ac.visualize()
