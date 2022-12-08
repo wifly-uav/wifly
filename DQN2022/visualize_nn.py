91bf7e7a73100d22ecba93eeb4e0e8d962e2cdea
Ok-Rk <riku3141@gmail.com>
2022-12-09 01:34:28 +0900

    a

--- DQN2022/visualize_nn.py
+++ DQN2022/visualize_nn.py
@@ -3,12 +3,13 @@ from PIL import Image, ImageDraw, ImageFont
 import os
 
 class visual_nn:
-    def __init__(self, flag=False, folder='log', max=[255,255,180,9], frame=4, norm=False):
+    def __init__(self, flag=False, folder='log', max=[255,255,180,9], frame=4, norm=False, mode=None):
         self.flag = flag
         self.folder = folder
         self.max = max
         self.frame = frame
         self.norm = norm
+        self.mode = mode
 
     def visualize(self):
         circle_size = 60
@@ -17,9 +18,18 @@ class visual_nn:
         b_bias = 1
         max_line = 30
         max_ell = 10
+        rnd_norm = [1,50,1]
+        con_norm = 10
 
         th_max = 10000
         th_min = 0
+
+        if self.mode == "RND":
+            W_rnd = np.loadtxt(self.folder + '/debug_W_fc_add.csv', delimiter=',')
+            rnd_layer = W_rnd.shape[0]
+        elif self.mode == "CON":
+            W_con = np.loadtxt(self.folder + '/debug_W_fc_con.csv', delimiter=',')
+            con_layer = 1
 
         W_fc1 = np.loadtxt(self.folder + '/debug_W_fc1.csv', delimiter=',')
         W_fc2 = np.loadtxt(self.folder + '/debug_W_fc2.csv', delimiter=',')
@@ -49,6 +59,10 @@ class visual_nn:
         middle_1 = np.zeros((middle_layer_1,2))
         middle_2 = np.zeros((middle_layer_1,2))
         output = np.zeros((output_layer,2))
+        if self.mode == "RND":
+            rnd_input = np.zeros((rnd_layer,2))
+        if self.mode == "CON":
+            con_input = np.zeros((con_layer,2))
 
         im = Image.new("RGB", (10240,5400), (128,128,128))
         draw = ImageDraw.Draw(im)
@@ -57,11 +71,29 @@ class visual_nn:
         start_x = width_x/2-circle_size/2
 
         #input_layer
-        height = im.height/input_layer
+        if self.mode == "RND":
+            height = im.height/(input_layer+rnd_layer)
+        elif self.mode == "CON":
+            height = im.height/(input_layer+con_layer)
+        else:
+            height = im.height/input_layer
         width = start_x
         for i in range (input_layer):
             input[i][0] = width+circle_size/2
-            input[i][1] = height*(i+0.5)+circle_size/2-circle_size/2
+            if self.mode == "RND":
+                input[i][1] = (height+rnd_layer)*(i+0.5)+circle_size/2-circle_size/2
+            elif self.mode == "CON":
+                input[i][1] = (height+con_layer)*(i+0.5)+circle_size/2-circle_size/2
+            else:
+                input[i][1] = height*(i+0.5)+circle_size/2-circle_size/2
+        if self.mode == "RND":
+            for i in range (rnd_layer):
+                rnd_input[i][0] = width+circle_size/2
+                rnd_input[i][1] = height*(i+0.5)+circle_size/2-circle_size/2
+        if self.mode == "CON":
+            for i in range (con_layer):
+                con_input[i][0] = width+circle_size/2
+                con_input[i][1] = height*(i+0.5)+circle_size/2-circle_size/2
         #print(input)
         #print(input[0][0])
 
@@ -95,7 +127,23 @@ class visual_nn:
                 weight_bias_ = abs(self.max[i%state_val])
                 for j in range (middle_layer_1):
                     W_fc1[i][j] = W_fc1[i][j]*weight_bias_
-            max_w1 = W_fc1.max()
+            if self.mode == "RND":
+                for i in range (rnd_layer):
+                    weight_bias_ = abs(rnd_norm[i])
+                    for j in range (middle_layer_1):
+                        W_rnd[i][j] = W_rnd[i][j]*weight_bias_
+            elif self.mode == "CON":
+                for i in range (con_layer):
+                    weight_bias_ = abs(con_norm)
+                    for j in range (middle_layer_1):
+                        W_con[j] = W_con[j]*weight_bias_
+
+            if self.mode == "RND":
+                max_w1 = max(W_fc1.max(),W_rnd.max())
+            elif self.mode == "CON":
+                max_w1 = max(W_fc1.max(),W_con.max())
+            else:
+                max_w1 = W_fc1.max()
             weight_bias1 = max_line/max_w1
         else:
             weight_bias1 = weight_bias
@@ -105,6 +153,21 @@ class visual_nn:
                     draw.line((input[i][0], input[i][1], middle_1[j][0], middle_1[j][1]), fill=(255, 0, 0), width=int(W_fc1[i][j]*weight_bias1))
                 else:
                     draw.line((input[i][0], input[i][1], middle_1[j][0], middle_1[j][1]), fill=(0, 0, 255), width=-int(W_fc1[i][j]*weight_bias1))
+        
+        if self.mode == "RND":
+            for i in range (rnd_layer):
+                for j in range (middle_layer_1):
+                    if W_rnd[i][j] > 0.0:
+                        draw.line((rnd_input[i][0], rnd_input[i][1], middle_1[j][0], middle_1[j][1]), fill=(255, 0, 0), width=int(W_fc1[i][j]*weight_bias1))
+                    else:
+                        draw.line((rnd_input[i][0], rnd_input[i][1], middle_1[j][0], middle_1[j][1]), fill=(0, 0, 255), width=-int(W_fc1[i][j]*weight_bias1))
+        elif self.mode == "CON":
+            for i in range (con_layer):
+                for j in range (middle_layer_1):
+                    if W_con[j] > 0.0:
+                        draw.line((con_input[i][0], con_input[i][1], middle_1[j][0], middle_1[j][1]), fill=(255, 0, 0), width=int(W_fc1[i][j]*weight_bias1))
+                    else:
+                        draw.line((con_input[i][0], con_input[i][1], middle_1[j][0], middle_1[j][1]), fill=(0, 0, 255), width=-int(W_fc1[i][j]*weight_bias1))
 
         #line_middl1_1_to_middle_2
         for i in range (middle_layer_1):
@@ -129,15 +192,28 @@ class visual_nn:
 
 
         #input_layer
-        height = im.height/input_layer
+        if self.mode == "RND":
+            height = im.height/(input_layer+rnd_layer)
+        elif self.mode == "CON":
+            height = im.height/(input_layer+con_layer)
+        else:
+            height = im.height/input_layer
         width = start_x
         for i in range (input_layer):
             color = (int)(i/state_val)
-            draw.ellipse((width, height*(i+0.5)-circle_size/2, width+circle_size, height*(i+0.5)+circle_size-circle_size/2), fill=(int(color*240/self.frame), int((self.frame-color)*240/self.frame), 100))
-            input[i][0] = width+circle_size/2
-            input[i][1] = height*(i+0.5)+circle_size/2-circle_size/2
-        #print(input)
-        #print(input[0][0])
+            if self.mode == "RND":
+                draw.ellipse((width, height*(i+rnd_layer+0.5)-circle_size/2, width+circle_size, height*(i+rnd_layer+0.5)+circle_size-circle_size/2), fill=(int(color*240/self.frame), int((self.frame-color)*240/self.frame), 100))
+            elif self.mode == "CON":
+                draw.ellipse((width, height*(i+con_layer+0.5)-circle_size/2, width+circle_size, height*(i+con_layer+0.5)+circle_size-circle_size/2), fill=(int(color*240/self.frame), int((self.frame-color)*240/self.frame), 100))
+            else:
+                draw.ellipse((width, height*(i+0.5)-circle_size/2, width+circle_size, height*(i+0.5)+circle_size-circle_size/2), fill=(int(color*240/self.frame), int((self.frame-color)*240/self.frame), 100))
+        if self.mode == "RND":
+            for i in range (rnd_layer):
+                draw.ellipse((width, height*(i+0.5)-circle_size/2, width+circle_size, height*(i+0.5)+circle_size-circle_size/2), fill=(0, 0, 100))
+        if self.mode == "CON":
+            for i in range (con_layer):
+                draw.ellipse((width, height*(i+0.5)-circle_size/2, width+circle_size, height*(i+0.5)+circle_size-circle_size/2), fill=(0, 0, 100))
+
 
         #middle_layer_1
         height = im.height/middle_layer_1
@@ -146,8 +222,7 @@ class visual_nn:
             circle_size_1 = circle_size + b_fc1[i]*b_bias
             #print(b_fc1[i])
             draw.ellipse((width, height*(i+0.5)-circle_size_1/2, width+circle_size_1, height*(i+0.5)+circle_size_1-circle_size_1/2), fill=(0, 255, 0, 128))
-            middle_1[i][0] = width+circle_size_1/2
-            middle_1[i][1] = height*(i+0.5)+circle_size_1/2-circle_size_1/2
+
 
         #middle_layer_2
         height = im.height/middle_layer_2
@@ -156,8 +231,7 @@ class visual_nn:
             circle_size_2 = circle_size + b_fc2[i]*b_bias
             #print(b_fc2[i])
             draw.ellipse((width, height*(i+0.5)-circle_size_2/2, width+circle_size_2, height*(i+0.5)+circle_size_2-circle_size_2/2), fill=(0, 255, 0))
-            middle_2[i][0] = width+circle_size_2/2
-            middle_2[i][1] = height*(i+0.5)+circle_size_2/2-circle_size_2/2
+
 
         #output_layer
         height = im.height/output_layer
@@ -166,8 +240,6 @@ class visual_nn:
             circle_size_output = circle_size# + b_out[i]*b_bias
             #print(b_out[i])
             draw.ellipse((width, height*(i+0.5)-circle_size_output/2, width+circle_size_output, height*(i+0.5)+circle_size_output-circle_size_output/2), fill=(0, 255, 255))
-            output[i][0] = width+circle_size_output/2
-            output[i][1] = height*(i+0.5)+circle_size_output/2-circle_size_output/2
 
         if self.flag:
             im.show()
@@ -178,7 +250,9 @@ if __name__ == "__main__":
     path = os.path.dirname(__file__)
     print('folder name:')
     save_folder = input()
-    save_file = os.path.join(path, 'result', save_folder)
+    save_file = os.path.join(path, 'result/syuron/', save_folder)
+    print('mode?')
+    mode = input()
 
-    vi = visual_nn(flag=True,folder=save_file, norm=True,frame=4,max=[255,255,90,1,50])
+    vi = visual_nn(flag=True,folder=save_file, norm=True,frame=4,max=[255,255,90,1,50],mode=mode)
     vi.visualize()
