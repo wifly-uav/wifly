bc0e4045a8c0fec7b9f6b0e668ca80c61402dc04
Ok-Rk <riku3141@gmail.com>
2023-01-11 17:33:18 +0900

    a

--- analyze/てこクランク/sim - コピー.py
+++ analyze/てこクランク/sim - コピー.py
@@ -12,11 +12,11 @@ import pyqtgraph as pg
 import os
 
 SIMULATOR_WINDOW_NAME = 'link simulator'
-FRAME_WIDTH = 10
+FRAME_WIDTH = 1
 NODE_RADIUS = 15
 NODE_WIDTH = 5
 FRAME_COLOR = (0, 0, 0)
-POSE_TO_PXL = 0.5
+POSE_TO_PXL = 500
     
 def drawFrame(self, img, color=FRAME_COLOR):
 
@@ -25,41 +25,68 @@ def drawFrame(self, img, color=FRAME_COLOR):
     else:
         self.Height, self.Width = img.shape[:2]
 
-    px0 = int(self.x0 * POSE_TO_PXL)
-    py0 = self.Height - int(self.y0 * POSE_TO_PXL)
-    px1 = int(self.x1 * POSE_TO_PXL)
-    py1 = self.Height - int(self.y1 * POSE_TO_PXL)
+
     cv2.line(img, (px0,py0), (px1,py1), color, FRAME_WIDTH)
 
 if __name__ == '__main__':
     path = os.path.dirname(__file__)
+    mode = 0
 
-    outst = np.loadtxt(path+'/outst.csv', delimiter=',')
-    inst = np.loadtxt(path+'/inst.csv', delimiter=',')
+    outst = np.loadtxt(path+'/outst.csv', delimiter=',', encoding="utf-8-sig")
+    inst = np.loadtxt(path+'/inst.csv', delimiter=',', encoding="utf-8-sig")
+    print(outst.shape)
 
-    bias = 8
-    measure_time = 3
-    mem = 10000
     while True:
 
         img = np.zeros((800,1200,3), np.uint8)
         img[:,:,:] = 255
 
+        if len(img.shape) == 3:
+            Height, Width, channels = img.shape[:3]
+        else:
+            Height, Width = img.shape[:2]
 
-        (x0, y0) = (200, 600)
+        (x0, y0) = (400, 500)
 
-        f1.drawFrame(img)
-        f2.drawFrame(img)
-        f3.drawFrame(img)
-        f4.drawFrame(img,(255,0,0))
-        f5.drawFrame(img)
-        f2_.drawFrame(img)
-        f3_.drawFrame(img)
-        f4_.drawFrame(img,(0,255,0))
+        if mode:
+            for i in range(len(outst)):
+                if i%100==0:
+                    x1 = x0+(outst[i][1]/180*pi)*0.5*POSE_TO_PXL
+                    y1 = y0
+                    x2 = x1+sin(outst[i][0]/180*pi)*0.2*POSE_TO_PXL
+                    y2 = y1-cos(outst[i][0]/180*pi)*0.2*POSE_TO_PXL
+                    print(outst[i][0])
+                    #print(str(x1)+','+str(y1))
+                    #print(str(x2)+','+str(y2))
+                    px0 = int(x1)
+                    py0 = Height - int(y1)
+                    px1 = int(x2)
+                    py1 = Height - int(y2)
+                    #print(str(px0)+','+str(py0))
+                    #print(str(px1)+','+str(py1))
+                    cv2.line(img, (px0,py0), (px1,py1), FRAME_COLOR, FRAME_WIDTH)
+        else:
+            for i in range(len(inst)):
+                if i%100==0:
+                    x1 = x0+(inst[i][1]/180*pi)*0.5*POSE_TO_PXL
+                    y1 = y0
+                    x2 = x1+sin(inst[i][0]/180*pi)*0.2*POSE_TO_PXL
+                    y2 = y1-cos(inst[i][0]/180*pi)*0.2*POSE_TO_PXL
+                    #print(str(x1)+','+str(y1))
+                    #print(str(x2)+','+str(y2))
+                    px0 = int(x1)
+                    py0 = Height - int(y1)
+                    px1 = int(x2)
+                    py1 = Height - int(y2)
+                    #print(str(px0)+','+str(py0))
+                    #print(str(px1)+','+str(py1))
+                    cv2.line(img, (px0,py0), (px1,py1), FRAME_COLOR, FRAME_WIDTH)
+
+        #f1.drawFrame(img)
 
         cv2.imshow(SIMULATOR_WINDOW_NAME, img)
 
-        if cv2.waitKey(1) > 0:
+        if cv2.waitKey(0) > 0:
             break
 
     cv2.destroyAllWindows()
