207061d1d12e9c6a3076986f76d7b191fff014e8
Ok-Rk <riku3141@gmail.com>
2023-03-26 02:20:06 +0900

    last

---
+++ ESP/ESP-EYE/esp-eye_rtsp/src/src/wifi_config.h
@@ -0,0 +1,6 @@
+const char *ssid = "UAV-ASUS";
+const char *password = "watanabe1408";
+
+const IPAddress ip(192, 168, 0, 100);
+const IPAddress gatway(192, 168, 0, 1);
+const IPAddress subnet(255, 255, 255, 0);
