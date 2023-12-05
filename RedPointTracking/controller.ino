2366a97d7e99fdbd789d6fae290552d0e573571f
H16kq <sato-74h56@fuji.waseda.jp>
2023-12-05 15:01:27 +0900

    機体とコントローラーのコード追加

---
+++ RedPointTracking/controller.ino
@@ -0,0 +1,386 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+#include <SPI.h>
+// #define DEBUG
+
+unsigned long lastTime = 0;  
+unsigned long timerDelay = 60;  // send readings timer
+size_t data_pc_;                //size_t:オブジェクトのバイト数を表現できる程度に十分に大きい符号なし整数型
+uint8_t data_pc[5] = {0};
+int re_data[5] = {0};
+double re_data_angle[4] = {0};
+
+// REPLACE WITH RECEIVER MAC Address
+
+//機体側のマイコンの番号にあったアドレスのみコメントアウトを外す。
+//uint8_t castAddress[] = {0x8C, 0xCE, 0x4E, 0xEA, 0xB1, 0xC9}; //0
+//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0xA1, 0x60}; //旧1
+//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0x81, 0x0A};   //新1
+//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0xA1, 0x3D}; //2
+//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0x95, 0xE4}; //3
+//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0xA1, 0xAC}; //4
+//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0x95, 0x98}; //5
+//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0x80, 0xA6}; //6　修正済み
+//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0xA3, 0x2D}; //7
+//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0x81, 0x89}; //9 李さん作
+//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0x80, 0x90}; // 10
+uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0x8D, 0x57}; //11 神永作
+//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0x90, 0xA9}; //12
+//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0x80, 0x34}; //13 
+//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F, 0x82, 0xF2}; // SHE
+//uint8_t castAddress[] = {0xB4, 0xE6, 0x2D, 0x2F,0x86, 0xB4}; // 李さん作
+
+esp_now_peer_info_t peerInfo;
+
+const int controller_num = 2; //1:A 2:B　変数の値を変更せずに定数として宣言　書き換えできず読み取り専用
+
+//pin
+//各ボタンのピン番号
+int stick_lr;
+int stick_ud;
+int slider_l;
+int slider_r;
+int switch_1;
+int switch_2;
+int volume;
+
+double ysqr = 0;
+double t0,t1,t2,t3,t4 = 0;
+int eular[3] = {0};
+
+//quotanion（四元数）をEuler角に変換する関数
+void qu2eu(int eular[3], double quotanion[4]){
+  double w = quotanion[0];
+  double x = quotanion[1];
+  double y = quotanion[2];
+  double z = quotanion[3];
+
+  double sz = -(2 * x * y - 2 * z * w);  
+  double a = asin(sz);
+  int flag = cos(a) != 0;
+  eular[0] = flag ? atan2(2 * y * z + 2 * x * w, 2 * w * w + 2 * y * y - 1)/PI*180 : atan2(-(2 * y * z - 2 * x * w), 2 * w * w + 2 * z * z - 1)/PI*180;
+  eular[1] = flag ? atan2(2 * x * z + 2 * y * w, 2 * w * w + 2 * x * x - 1)/PI*180 : 0;
+  eular[2] = a/PI*180;
+
+  /*
+  ysqr = y * y;
+
+  // roll (x-axis rotation)
+  t0 = +2.0 * (w * x + y * z);
+  t1 = +1.0 - 2.0 * (x * x + ysqr);
+  eular[0] = atan2(t0, t1)/PI*180;
+
+  // pitch (y-axis rotation)
+  t2 = +2.0 * (w * y - z * x);
+  t2 = t2 > 1.0 ? 1.0 : t2;
+  t2 = t2 < -1.0 ? -1.0 : t2;
+  eular[1] = asin(t2)/PI*180;
+
+  // yaw (z-axis rotation)
+  t3 = +2.0 * (w * z + x * y);
+  t4 = +1.0 - 2.0 * (ysqr + z * z);  
+  eular[2] = atan2(t3, t4)/PI*180;
+  */
+}
+
+//受信時コールバック関数（データを受信すると実行される）
+//引数:送信元macアドレス情報,受信データ,受信データ長
+void onReceive(const uint8_t* mac_addr, const uint8_t* data, int data_len) {
+    //noInterrupts();
+    //taskDISABLE_INTERRUPTS();
+    char macStr[18];
+    //データの送信元のmacアドレスを整形し（%02X:等を付け）て、macStr配列に書き込む。
+    //%02Xは2桁以上の16進数で表示することを指定（桁が足りない場合、上位の桁が0埋めされる。) MACアドレスは0~9、A~Fは16新数でA0:B2のように表示される
+    snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X", 
+        mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+    //Serial.println();
+    //Serial.printf("ESP32\n");
+    //Serial.printf("Last Packet Recv from: %s\n", macStr);
+    //Serial.printf("Last Packet Recv Data(%d): ", data_len);
+    //Serial.println();
+
+    //受信データの整形
+    //data_lenは9
+    //[pwm1, pwm2, servo1, servo2, 受信間隔, quaternion1,quaternion2,quaternion3,quaternion4]
+    for (int i = 0; i < data_len; i++) {
+        if(i > 4){
+          //受信データの5番目以降（quotanion）は、処理してre_data_angleに格納していく。
+          re_data_angle[i-5] = data[i]*0.01-1;
+        }else if(i < 2){
+          //受信データの先頭2つ（羽ばたき出力）は、0~255を反転してre_dataに格納
+          re_data[i] = 255 - data[i];
+        }
+        else{
+          //残り(受信間隔とservo1または2の二つ)はそのままre_dataに格納(ただしservo1,2はつないぐとき尾翼servoと重心移動機構のどちらかは変わる)
+          re_data[i] = data[i];
+        }
+    }
+    
+    //カンマ区切りでre_dataの中身をシリアルポートに送信
+    for (int i = 0; i < 5; i++) {
+      Serial.print(re_data[i]);
+      if(i != 4){
+        Serial.print(",");    //データの最後にカンマを打たないようにするため
+      }
+    }
+
+    //受信したquotarnionをEuler角に変換
+    qu2eu(eular,re_data_angle);
+    Serial.print(",");
+
+    //Euler角（Pitch, Yaw, Roll）をシリアルポートに送信
+    for (int i = 0; i < 3; i++) {
+      Serial.print(eular[i]);
+      if(i != 2){
+        Serial.print(",");
+      }
+    }
+    Serial.println();           
+    //Serial.flush();     //データを送信しきるまで（送信バッファが空になるまで）待つ。
+    delay(10);            //flushじゃだめかもしれない？
+    //interrupts();
+    //taskENABLE_INTERRUPTS();
+    //delay(10);           //さらに待機 受信処理が終わるまで待っている
+}
+
+//送信時コールバック関数
+//送信が成功したかどうかを表示する。
+void OnDataSent(const uint8_t* mac_addr, esp_now_send_status_t status) {
+  #ifdef DEBUG
+    Serial.println();
+    Serial.printf("ESP32\n");
+    Serial.print("Last Packet Send Status: ");
+    if (status == 0){
+      Serial.println("Delivery success");
+    }
+    else{
+      Serial.println("Delivery fail");
+    }
+  #endif
+}
+
+//PCからの情報を受信する関数
+void recieve_pc(){
+  if(Serial.available()){                               //シリアルポートにデータが来ている場合
+    if((int)Serial.read() == 255){                      //データの先頭が255である場合
+
+      //5つのデータを読み取り、data_pcに格納する。
+      //data_pc_にはデータ長が格納される
+      data_pc_ = Serial.readBytesUntil('\n',data_pc,5);
+      delay(10);  // dataを読み切るための待機時間を追加 
+    }
+  }
+}
+
+void setup() {
+
+  //各コントローラごとのピン番号
+  switch(controller_num){
+    case 1:
+      stick_lr = 34;  // マイコンのどの番号とつながっているかを表している
+      stick_ud = 35;
+      slider_l = 33;
+      slider_r = 32;
+      switch_1 = 23;
+      switch_2 = 13;
+      volume = 39;
+      break;        //忘れずに!
+    case 2:
+      stick_lr = 36;
+      stick_ud = 39;
+      slider_l = 33;
+      slider_r = 32;
+      switch_1 = 23;
+      switch_2 = 13;
+      volume = 39;
+      break;
+    }
+
+    //シリアル通信開始
+    //Serial.begin(460800);   もとのボーレート
+    //Serial.begin(115200);   一段階下げたもの
+    //9600, 14400, 19200, 28800,38400
+    //Serial.begin(57600);    もう一段階下げたもの（Lazurite時代と同じ）
+    Serial.begin(38400);  // 転送レートの初期化,つまり転送の準備を開始
+    while(!Serial); // 上の転送の準備ができるのを待つ
+    
+    
+    //ピンモードの設定
+    pinMode(stick_lr, INPUT); // デジタルピン番号を入力に設定
+    pinMode(stick_ud, INPUT);
+    pinMode(slider_l, INPUT);
+    pinMode(slider_r, INPUT);
+    pinMode(volume, INPUT);
+    pinMode(switch_1, INPUT_PULLUP);  //トグルスイッチのプルアップ、ダウンはここで設定
+    pinMode(switch_2, INPUT_PULLUP);
+
+    Serial.println(WiFi.macAddress());
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+
+    if (esp_now_init() == ESP_OK) { // esp_now_init()を呼び出し初期化、これが成功したか確認するための条件文
+        //Serial.println("ESP-Now Init Success");
+    }
+      // Register peer
+    memcpy(peerInfo.peer_addr, castAddress, 6); // おまじない
+    peerInfo.channel = 0;  
+    peerInfo.encrypt = false;
+    
+    // Add peer        
+    if (esp_now_add_peer(&peerInfo) != ESP_OK){ // esp_now_add_peer()で設定をESP-NOWに適用するが、失敗したときにメッセージを表示する
+      Serial.println("Failed to add peer");
+      return;
+    }
+    
+    //esp_now_register_send_cb(OnDataSent);
+    //受信時コールバック関数の指定
+    esp_now_register_recv_cb(onReceive);  // esp_now_init()で初期化をして、esp_now_register_recv_cb()でデータが受信されたときに実行する関数を登録する
+    
+}
+  //setup終わってからloopが走る
+//各アナログスイッチ入力値格納用
+int left_LR;
+int left_UD;
+int sli_L;
+int sli_R;
+int vol;
+
+void loop() {
+  
+  //トグルスイッチの入力値読み取り
+  int btn_L = digitalRead(switch_1);  // スイッチ1の値をボタンLが読み取る(値は0or1)
+  int btn_R = digitalRead(switch_2);  // スイッチ2の値をボタンRが読み取る(値は0or1)
+
+  if ((millis() - lastTime) > timerDelay) { // 受信間隔がtimerDelay以上であれば、(setupからの実行時間からif文が終わった時間を引いたものが受信間隔)　lastTime=millisとなるため大きくなるので受信間隔はほぼ変わらない  millis():実行中のプログラムがスタートしてからの時間(setupも含む) 符号なしの整数型(unsigned long)
+    uint8_t data[5];  // 符号なし8bit整数型として、data[5]を宣言
+
+    //PCモード
+    if(btn_L == 1){
+      recieve_pc();                         //PCからデータ受信
+      //Serial.print("PC_mode");
+
+      //PCから受け取った情報を機体側のマイコンに送信する準備
+      for(int i=0;i<2;++i){
+        data[i] = 254 - data_pc[i];         //受信データの先頭2つ（羽ばたき出力）は反転 254の謎、なぜ255ではないのか？？
+      }
+      for(int i=2;i<5;++i){
+        data[i] = data_pc[i];
+      }
+    
+    //コントローラモード
+    }else{  
+    
+      switch(controller_num){
+        case 1: //コントローラーA
+          left_LR = map(analogRead(stick_lr),0,4096,0,255); //mapは整数のみを扱う(小数点以下は切り捨て)　つまりリマッピング関数　コントローラーの値が0～4096、これを機体の数字0~255に変換　数が収まるようにまずstick_lrの値が連続的に読まれたものをLeft_LRに代入。割り当てなし。
+          left_UD = map(analogRead(stick_ud),0,4096,0,180); // 尾翼サーボ
+          sli_L = map(analogRead(slider_l),0,4096,0,255); // 羽ばたき出力　(臨機応変に左右変更)　コードが変ならつなぎなおす
+          sli_R = map(analogRead(slider_r),0,4096,0,255); // 羽ばたき出力　(臨機応変に左右変更)　コードが変ならつなぎなおす
+          vol = map(analogRead(volume),0,4096,0,255);
+          //btn_R = digitalRead(switch_2);
+
+          if(left_LR<152 && left_LR>102){left_LR = 127;}  //　スティックの誤差を修正する(左右)、デッドゾーン補正
+          if(left_UD<105 && left_UD>75){left_UD = 90;}  //　スティックの誤差を修正(上下)、デッドゾーン補正
+
+          data[0] = sli_L;
+          data[1] = sli_R;
+          data[2] = left_UD;
+          data[3] = btn_R*120;  // 右ボタンが0または1で、0度なら水平飛行の態勢、ボタンが1となると90度を180分割して2度が1度となるので実際機体においての60度がプログラム内では120度となる
+          data[4] = btn_R;
+          break;
+        
+        case 2: // コントローラーB
+          left_LR = map(analogRead(stick_lr),0,4096,0,255); //　揚力差　mapは整数のみを扱う(小数点以下は切り捨て)　つまりリマッピング関数　コントローラーの値が0～4096、これを機体の数字0~255に変換　数が収まるようにまずstick_lrの値が連続的に読まれたものをLeft_LRに代入
+          left_UD = map(analogRead(stick_ud),0,4096,0,255); //　割り当てなし
+          sli_L = map(analogRead(slider_l),0,4096,0,180);   //　尾翼サーボ　数字の255と180自体に意味はない、ただし尾翼なら最大数値が180ということ　case1でも180が尾翼サーボとなっている
+          sli_R = map(analogRead(slider_r),0,4096,0,255);   //　羽ばたき出力
+          //btn_L = digitalRead(switch_1);
+          //vol = map(analogRead(volume),0,4096,0,255);
+          //btn_R = digitalRead(switch_2);
+
+          //スライドボリューム左（尾翼）最小
+          if(sli_L>170){sli_L = 180;} //　左スライドの値が170より大きければ、左スライドに180を代入、スライドの誤差調整
+          //スライドボリューム右（羽ばたき出力）最小
+          if(sli_R>240){sli_R = 255;} //　右スライドの値が240よりも大きければ、右スライドに255を代入、スライドの誤差調整
+          //ジョイスティック左右中央
+          if(left_LR<152 && left_LR>102){left_LR = 127;}  //　スティックの誤差を修正する(左右)、デッドゾーン補正
+          //ジョイスティック上下中央
+          if(left_UD<152 && left_UD>102){left_UD = 127;}  //　スティックの誤差を修正(上下)、デッドゾーン補正
+
+          /*
+          int left_pwm = abs(sli_R - 255) - (left_LR - 127)/4;  // 左（要確認）absはアブソリュート関数のこと、数値の政府は関係なく絶対値を求める関数　二つの数値の差分を求める。今なら左スライドの値から255を引いたものの絶対値をとるということ
+          int right_pwm = abs(sli_R - 255) + (left_LR - 127)/4; // 右（要確認）absはアブソリュート関数のこと、数値の政府は関係なく絶対値を求める関数　二つの数値の差分を求める。今なら右スライドの値から255を引いたものの絶対値をとるということ
+          int servo_angle = abs(sli_L-180);                     // 尾翼サーボ角 absはアブソリュート関数のこと、数値の政府は関係なく絶対値を求める関数　二つの数値の差分を求める。今なら左スライドの値から180を引いたものの絶対値をとるということ
+          */
+
+          int left_pwm = max(0,sli_R - max(0, (left_LR - 127)/4 ));  // 左（要確認）揚力差は0~255の値に変換されているので127を引いたら負の値になることもある、そのときは0が出されるまたは揚力差から127引いて4で割った値が出される。その値をスライド右の値から引く、その値をまた0と比較して大きい値をleft_pwm(この関数の意味はモーターの出力を変えて左に行ったりできるように制御できるようにするもの)に代入
+          int right_pwm = max(0,sli_R - max(0, (127 - left_LR)/4)); // 右（要確認）left_LRが前のコードで代入されることによって(スティックを右に傾けるとか左に傾けるとか)127からLeft_LRの値を引く、もしくはLeft_LRを127で引くことによって片方は0より小さくなり、もう一つは0より大きくなる。0よりも大きくなったほうは右スライド(コントローラー)である羽ばたき出力からその揚力差の値が引かれるため、片方の羽ばたき出力が小さくなり右or左に機体が傾く。
+          int servo_angle = sli_L;                   // 尾翼サーボ角 左スライドの値を尾翼サーボの角度という変数に代入
+          int cog_angle = 120*btn_R;                 // 重心移動機構の角度 cogとはcenter of gravity 40→1120に変更した。右ボタンが0または1で、0度なら水平飛行の態勢、ボタンが1となると90度を180分割して2度が1度となるので実際機体においての60度がプログラム内では120度となるようにするため120をかけている。
+
+          data[0] = left_pwm;          
+          data[1] = right_pwm;
+          data[2] = servo_angle;
+          data[3] = cog_angle;
+
+          break;
+      }
+    }
+    
+    //羽ばたき出力の矯正
+    //大体Maxの値になっていたらMaxの出力にしてやる
+    if(data[0]>240){data[0] = 255;} // 機体の羽ばたき出力が240のとき、右スライドの位置(一番上との間隔)はほぼ機体の羽ばたき出力が255のときと数mmの差しかない、なので255に補正してしまう。
+    if(data[1]>240){data[1] = 255;} // 機体の羽ばたき出力が240のとき、右スライドの位置(一番上との間隔)はほぼ機体の羽ばたき出力が255のときと数mmの差しかない、なので255に補正してしまう。
+
+    //コントローラモードの場合のみ　PCモードの時はデバックするとPCで受信した値とデバックした値が混ざるのでコントローラーモードのみで実行
+    if(btn_L != 1){ // btn_Lの1でないとき　ボタンLの0or1でPCモードとコントローラーモードを区別する、このプログラムでは0がコントローラーモード
+      //PC側にコントローラから指示された値を返してやる。
+      //PC側にシリアル通信で送信することで、シリアルモニタに表示される。
+      //PC_modeでこれをやったらダメ!receive_from_espがバグる。
+      #ifdef DEBUG  // 5行目のDEBUGがdefineされていれば、#ifdef~#endifまでを実行、今は5行目がコメントアウトされているためoffとなっている。それぞれのdataにそれぞれの値が正確に入っているか確認したいだけ
+        /*
+        Serial.print("lx:");
+        Serial.print(left_LR);
+        Serial.print("ly:");
+        Serial.print(left_UD);
+        Serial.print("sli_L:");
+        Serial.print(sli_L);
+        Serial.print("sli_R:");
+        Serial.print(sli_R);
+        Serial.print("vol:");
+        Serial.print(vol);
+        Serial.print("btn1:");
+        Serial.print(btn_R);
+        Serial.print("btn2:");
+        Serial.print(btn_L);
+        */
+        Serial.print(data[0]);
+        Serial.print(data[1]);
+        Serial.print(data[2]);
+        Serial.print(data[3]);
+        Serial.println();
+        Serial.print(btn_R);
+        /*
+        Serial.print("rx:");
+        Serial.print(right_LR);
+        Serial.print("ry:");
+        Serial.print(right_UD);
+        Serial.print("btnr:");
+        Serial.print(btn_r);
+        Serial.print("btnl");
+        Serial.print(btn_l);
+        Serial.println();
+        */
+      #endif
+    }
+
+    //delay(10);  // このdelayは気にしないでよい
+
+    // Send message via ESP-NOW
+    esp_now_send(castAddress, (uint8_t *) &data, sizeof(data)); // esp_now_send()というのが呼び出されると、送信が実行。ここでカッコ内の変数は次のようになる。esp_now_send(送信先MACアドレス、送信するデータ、送信するデータのサイズ) (uint8_tは符号なし8bit整数型)　uint8_t *　これは符号なし8bit整数型のポインタ　(uint8_t *) &data　&dataはアドレス(値を格納しているもの)　uint8_t *は符号なしの８8bit整数型ポインタなので、&dataの中の値を符号なしの8bitの整数にしてくれということ。　dataは配列なのでそのまま変数として渡すことはできないのでポインタに変換して渡している、uint8_t dataはできない。送信先のMacアドレスCastaderessの中身の値の意味はマイコンごとの値(その値に意味はなく、マイコンごとの番地のようなものなのでマイコンによって決まる)　かつこれは配列なので中身の値が8bitの符号なし整数型ということ
+    delay(10);  // データを受け取る前に変な処理が走らないための、待機時間
+    // esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+    lastTime = millis();  // millis()とはマイコンが動作を開始してから累積時間を返す関数 unsigned long型の数値を返す 現在の経過時間を取得 if文の条件まで1回目はmillis()に累積されlastTImeを引く(ただし初回のみ)、そして条件文からlastTIme=millis()までの時間がmillis()にまた累積されている、これをlastTimeに代入。そしたら二回目以降はloop文まで戻る、そのloop文からif文までの時間がmillis()に累積される、そしてそのmillis()を先ほど代入したlastTimeで引き算、これを繰り返す。 ちなみにマイコンを停止すればこのコードは停止するのでmillis()のoverflowを使用することはない
+  }
+}
---
+++ RedPointTracking/kitai.ino
@@ -0,0 +1,255 @@
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+#include <Servo.h>
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+#include <SPI.h>
+
+#define DEBUG         // シリアルモニタで各スイッチの出力を確認できる。
+//#define sensor        // 操縦時はコメントアウト←これをワンチャンコメントアウトしてみる
+
+//int kitai_number = 4;   
+//1~5:Pch 6:Nch
+
+//コントローラごとにここを切り替える!!!!----------------------------------
+
+char controller = 'B';  //大きいコントローラ：A 小さいコントローラ：B
+
+//---------------------------------------------------------------------
+
+#define PWM_FREQ 1000 // FREQ(周波数)1000 PWM出力とはオンとオフの繰り返しスイッチングを行い、出力される電力を制御する 周波数が大きすぎると何がおきる？？(調べる、理由は保留) 周波数が高くなりすぎるとデューティ比の0近くと1023近くで挙動がおかしくなる Source:https://www.ei.tohoku.ac.jp/xkozima/lab/espTutorial0.html
+#define PWM_RANGE 255 // PWM値の制限をかけている、最大が大きすぎないようにする
+
+#ifdef sensor
+  Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);  // センサーの初期化????
+#endif
+
+//各種ピン番号設定
+const int pwm1 = 12;
+const int pwm2 = 13;
+const int pwmch = 0;
+const int cog_pin = 16;
+const int ladder_pin = 14;
+const int led = 15;
+
+//Servoクラスの作成
+Servo cog;  // 重心移動機構のServoクラスの作成
+Servo ladder; // 尾翼のServoクラスの作成
+
+int command[8] = {0}; // 配列0~7を初期化するために、すべて0埋め
+uint8_t data[9];  // 符号なし8bit整数型の配列、配列の中身は0~8の番号
+
+unsigned long lastTime = 0;     // 最新の受信時刻 符号なし長整数型 バイト長4
+unsigned long recvTime = 0;     // 受信時刻(未使用)
+unsigned long Ti = 0;
+unsigned long loopTi = 0;       // 受信間隔
+unsigned long timerDelay = 20;  // send readings timer
+unsigned long watchdogtime = 100;  // timer マイコンのプログラムが停止・暴走をしていないかを確認するためのタイマ
+
+double w,x,y,z = 0; // double型は実数を扱うことができる
+
+int i = 0;
+
+// Callback when data is sent
+// DEBUG時には送信が成功したかどうかを表示する。
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {  // メッセージが送信されるときに実行されるコールバック関数、この関数はメッセージが正常に受信されたかどうかを単純に出力する。 esp_now_sendが走ったらOndatasent実行　sendStatusはesp_now_sendが成功したかどうか0or1で判断 Macaddrは引数として必要で、値としては呼び出されるときに送信先のmacaddrが入る。 今回なら送信先のmacaddrはbroadcastaddressであり、そのアドレスが入る。結局はbraodcastadressの値となる。macAddrとしてもbroadcastAddrでも問題はない、関数内で使われる名前はそこで使われる名前なので送信する関数の名前と変わっても大丈夫。例えば数字a,b,c...を引き渡すとして関数内の引数をいちいちa,b,c...とか書いてられないからそこをnumとするとかがよい例である。ここでMacaddrをbroadcastaddrとしてもよい、それでも変わらない。ただし型を変えるのはだめ。
+  #ifdef DEBUG_SENT
+    Serial.println(); // 改行
+    Serial.print("Last Packet Send Status: ");  // "Last Packet Send Status: " 空白にはif文の中printlnの文字が続く
+  if (sendStatus == 0){
+    Serial.println("Delivery success");
+  }
+  else{
+    Serial.println("Delivery fail");
+  }
+  #endif
+}
+
+// Callback function that will be executed when data is received
+void OnDataRecv(uint8_t * mac_addr, uint8_t *data, uint8_t len) { // データが受信されたときに実行されるコールバック関数　lenというのはdata_lenであってるコントローラーから送られてくるデータの長さによって異なる(lenの値)
+  digitalWrite(led, HIGH);  // LED点灯 HIGHを引数で呼ぶことでLEDは暗く光る ledは37行目のconst led int = 15というもの これを入れることでそのピン番号に接続されているLEDが光るということ
+  //char macStr[18]; macStrという文字列の配列の定義であり、その文字列サイズが18である。
+  //snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
+  //    mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]); mac_addr[]の値を"%02X:%02X:%02X:%02X:%02X:%02X"の16進数の文字列に変換してmacStrという文字配列に書き込む、書き込む文字数はmacStrのサイズの文字数となっている(今回は18である) %02xは整数を16進数で出力する
+  #ifdef DEBUG
+    Serial.println(); //  改行
+    // Serial.printf("Last Packet Recv from: %s\n", macStr);  // %s\nは文字列を出力、今回はmacStrという文字列を出力
+    Serial.printf("Last Packet Recv Data(%d): ", len);  // %dは符号あり整数を表示、ここで矛盾があるのだが、lenは符号なしの8bit型の整数なのに出力は可能なのか？？→lenが正の値なので代入しても大丈夫
+  #endif
+
+  //データ受信
+  for (i = 0; i < len; ++i) {
+    command[i] = data[i]; // 43行目で定義されている配列0~7に44行目の配列の値を代入　dataの値ってどこに入ってる？？→これはOnDataRecvの*dataのことであり、これはポインタなので43行目のdata配列に格納される、もう一つの疑問としてはintの配列にuint8_tは代入できるの？？→intは大枠なのでuint8_tを代入することは可能
+    #ifdef DEBUG
+      Serial.print(data[i]);  // data[i]を出力して下の行でスペース出力
+      Serial.print(" ");
+    #endif
+  }
+  digitalWrite(led, LOW); // LED消灯（受信を繰り返すことでLEDが高速点滅）
+  recvTime = millis();  // ここまでのプログラムの実行時間をrecvTimeに代入、それが受信時刻となる　1行目からプログラムが走ってここまでの時間であってる？？→それで大丈夫
+}
+
+uint8_t broadcastAddress[6];  // 符号なし8bit整数型の配列、配列の中身は0~5となっている。
+void setup() {
+  Serial.begin(115200);               // シリアル通信開始（速度指定）
+  Serial.println();                   // 改行出力
+
+  pinMode(pwm1, OUTPUT);              // 羽ばたきモータ出力1に対応するピンを出力モードにする。ピン番号は32行目で12と設定されている
+  pinMode(pwm2, OUTPUT);              // 羽ばたきモータ出力2に対応するピンを出力モードにする。ピン番号は33行目で13と設定されている
+  pinMode(led, OUTPUT);               // LEDに対応するピンを出力モードにする。
+  analogWriteFreq(PWM_FREQ);          // アナログ出力の周波数を指定 PWM_FREQ24行目で定義
+  analogWriteRange(PWM_RANGE);        // アナログ出力の範囲を指定 PWM_RANGE25行目で定義
+
+  digitalWrite(led, HIGH);            // LED点灯
+  analogWrite(pwm1, PWM_RANGE);       // 羽ばたきを止める　pinMode関数を呼び出して出力に設定する必要はなし　analogWrite(pin, value)　pin出力したいピン番号、デューティ比0(常にオフ)~255(常にオン)　ここで使用可能なデータ型はintのみ　結局ピン番号とその上限値をしていしているのみ
+  analogWrite(pwm2, PWM_RANGE);       // 羽ばたきを止める
+  
+  cog.attach(cog_pin,900,1900);       // 重心移動機構サーボ出力の上限下限を設定　cog_pinは重心移動機構のあるピンの番号
+  ladder.attach(ladder_pin,900,1900); // 尾翼サーボ出力の上限下限を設定
+  
+  cog.write(0);                       // サーボの角度をリセット（0°）重心移動機構の角度 setupだから値を初期値の0に持っていきたい115行目も同様
+  ladder.write(0);                    // 尾翼サーボの角度をリセット
+
+  // REPLACE WITH RECEIVER MAC Address
+  // 各コントローラのMACアドレス
+  if(controller == 'A'){
+    broadcastAddress[0] = 0x8C; // 8Cの計算 Cは12であるこれ間違えないように!! 8C=8*16^1+(C=12)=140
+    broadcastAddress[1] = 0x4B;
+    broadcastAddress[2] = 0x14; // 0x14の計算 0X14=1*16+4*16^0=20
+    broadcastAddress[3] = 0x16;
+    broadcastAddress[4] = 0x63;
+    broadcastAddress[5] = 0x0C; // 表記的な疑問　0xが16進数という意味になるなら0を入れるのはなぜ？？ 0xの後ろには二桁必要なの？？→書式として必要
+  }else if(controller == 'B'){
+    broadcastAddress[0] = 0xC8; // 0xC8=(C=12)*16^1+8=200
+    broadcastAddress[1] = 0x2B;
+    broadcastAddress[2] = 0x96;
+    broadcastAddress[3] = 0xB9;
+    broadcastAddress[4] = 0x69;
+    broadcastAddress[5] = 0x54;
+  }
+  
+  #ifdef DEBUG
+    Serial.println("-");
+    Serial.println("start");
+
+    Serial.print("ESP8266 Board MAC Address:  "); // MAC Adddressの後には次の行が入る
+    Serial.println(WiFi.macAddress());  // この上の行のスペースに入るWiFi.macAddress()は何？？この値は→引数は()に何も入らない、けど書式として必要、ないと変数か関数かがわからないのでエラーがはかれる。
+  #endif
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);  // STA(ステーションモード)で動作、アクセスポイントに接続し、クラウドへデータを送信するのが一般的 wifi機能をステーションモードで起動 ステーションモードとはWi-Fi機器の動作モードの一つで、端末（無線LAN子機）としてWi-Fiアクセスポイント（AP）に接続するモード アクセスポイントとは無線でスマートフォン、ノートパソコン、ゲーム機といった複数の無線LAN（Wi-Fi）機能付きの端末を、家庭内LANに接続するための機械 
+
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {  // esp_now_init()はincludeでESP8266WiFi.hをやっているのでそこに含まれているから定義しなくても使用可能　そもそもesp_now_intit()はwifi機能を使用　もしesp_now_init()は0でないなら下のエラー文を出力
+    #ifdef DEBUG
+      Serial.println("Error initializing ESP-NOW");
+    #endif
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER); // set self roleってことは役割を決めてる？？その役割とは？？　上のCBとは何を示しているの？←これ不明保留
+  //esp_now_register_send_cb(OnDataSent); Esp-nowコールバック登録のsendの方　コメントアウトしている理由は？←これはOnDataSent関数がコメントの出力しかしないのでコメントアウトしている。また163行目の受信のほうはLEDの点滅として使用するのでこちらはコメントアウトせず
+  
+  // Register peer peerとはネットワーク通信において接続している相手のコンピューターや通信機器のこと。なのでこれを登録するということ
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0); // 引数何を取っている?? 保留
+
+  // 受信コールバック関数の指定
+  esp_now_register_recv_cb(OnDataRecv); // 引数には受信のコールバック関数を用いる
+
+  #ifdef sensor
+    if(!bno.begin()){ // センサを!bno.begin()でセンサの初期化を行う　ただ#define sensorをコメントアウトするとセンサが使えなくなってしまう
+      /* There was a problem detecting the BNO055 ... check your connections */ //BNO055とは角度センサのこと
+      #ifdef DEBUG
+        Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+      #endif
+      digitalWrite(led, HIGH);  // この下にwhile(1)があるのでこのライトは光り続ける、なぜならwhile(1)でコードが終わることはないから
+      while(1); //ここで無限ループする意味とは？？→これは失敗したら待機するようなもの、失敗しているのでコードを続けたくない、つまりコードをそこのままでとどめておきたいということ。
+    }
+  #endif
+
+  // 通信の初期設定に成功したら、LED(オレンジ?)を消す。
+  digitalWrite(led, LOW);
+}
+
+//float型用のmap関数を自作
+float mapfloat(float x, long in_min, long in_max, long out_min, long out_max) // 型名をfloatで宣言、関数名がmapfloat floatとは実数を扱うことのできるデータ型　ここでの実数は整数型と比べて値の桁数が多く少数も扱えるような数値となっている　引数mapfloat(変換したい数値、現在の範囲の下限、現在の範囲の上限、変換後の範囲の下限、変換後の範囲の上限) float型なのにその数値の現在の範囲の下限と上限がlong型とは？？　floatが少数も扱える変数型なのでlongで定義してもよいということ？？　保留して後で出てきた場合に確認 floatは4byte=16bitでありlong int(long)型も4byte=16bitであるのでもし今回の範囲が負になることはないから大枠というfloatの中にlong intがあるので変換したい変数がfloatでその範囲がlong intとなることは可能である。 http://www.isc.meiji.ac.jp/~mcelab/www_jyo_en2/jyo_jissu_2_cast.htm
+{
+  return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
+}
+ 
+void loop() {
+
+  #ifdef sensor
+    // Possible vector values can be:
+    // - VECTOR_ACCELEROMETER - m/s^2
+    // - VECTOR_MAGNETOMETER  - uT (マイクロテスラ)　1μTとは5Aの電流が流れている電線から1m離れた場所での磁界の大きさを表している　Magnetometer:磁力計
+    // - VECTOR_GYROSCOPE     - rad/s ジャイロスコープ
+    // - VECTOR_EULER         - degrees
+    // - VECTOR_LINEARACCEL   - m/s^2
+    // - VECTOR_GRAVITY       - m/s^2
+    //imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);　センサーフュージョンによる方向推定値の取得
+    imu::Quaternion quaternion = bno.getQuat(); // センサフュージョンの方向推定値のクオータニオン クオータニオンとは四元数と呼ばれ、二次元平面の回転を表現する複数の拡張として、3次元の回転を表現可能
+  #endif
+
+  Ti = millis();              // 現在時刻
+  loopTi = Ti - lastTime;     // 前回受信からの時間差 // 239行目でlastTime更新、最初はlastTime=0となっている
+  if (loopTi > timerDelay) {  // timerDelay=20よりも大きければif文が走る
+    // Set values to send
+    // 送信データを準備
+    data[0] = command[0];     // 羽ばたき出力1     43行目でcommandの配列が定義されている、dataを送信するため86行目において43行目でポインタによりdata配列に格納されたcommandの値を各データに代入
+    data[1] = command[1];     // 羽ばたき出力2
+    data[2] = command[2];     // 尾翼サーボ角度
+    data[3] = command[3];     // 重心移動機構角度
+    #ifdef sensor
+      data[5] = (quaternion.w()+1)*100;
+      data[6] = (quaternion.x()+1)*100;
+      data[7] = (quaternion.y()+1)*100;
+      data[8] = (quaternion.z()+1)*100;
+    #else
+      data[5] = 0; // 86行目でデータを受信する際にdata[]はcommand[]に代入したのでいまはdata[5~8]には値がないため、これに0を代入
+      data[6] = 0;
+      data[7] = 0;
+      data[8] = 0;
+    #endif
+    data[4] = loopTi;         // 受信間隔 201行目でloopTi=Ti-lastTimeとなっている loop文の中でloopTiとTiとlastTimeが代入されていくので結局受信間隔は変わらない
+    #ifdef DEBUG
+      //Serial.print("y:");　センサを初めから使用すると考えていないのかもしれないなのでEuler角のxが存在しない？？
+      //Serial.print(euler.y());
+      //Serial.print("z:");
+      /*
+      Serial.print(quaternion.w());
+      Serial.print(" , ");
+      Serial.print(quaternion.x());
+      Serial.print(" , ");
+      Serial.print(quaternion.y());
+      Serial.print(" , ");
+      Serial.print(quaternion.z());
+      Serial.println();
+      */
+    #endif
+
+    //　機体の状態をコントローラに送信
+    esp_now_send(broadcastAddress, (uint8_t *) &data, sizeof(data));  // OnDataRecv関数に送信され、OnDataRecv関数が受信したらその関数が走る、また送信の成功によってOnDatasent関数も走る
+    lastTime = millis();  // 今までのコードが走った時間をlastTimeに書きこむ
+  }
+  /*
+  if ((Ti - recvTime) > watchdogtime){　Ti(現在時刻)からrecvTime(受信時刻)を引いたものがタイマよりも大きいと暴走している可能性があるので
+    digitalWrite(led, HIGH);　LEDを光らせる
+    analogWrite(pwm1, 0);　羽ばたき出力モータ1を0にする
+    analogWrite(pwm2, 0);　羽ばたき出力モータ2を0にする
+  }
+  */
+  //受信データに基づき各出力を変更
+  analogWrite(pwm1, command[0]);  // 205~208でcommand[0~3]はdata[0~3]に代入しているが、それによってcommandの値は消えないから大丈夫!!、入れ替えの場合は上書きとなってしまう。
+  analogWrite(pwm2, command[1]);
+  ladder.write(command[2]); // 尾翼サーボ角度
+  cog.write(command[3]);  // 重心移動機構角度
+  
+  //delay(50); wait for a second
+}
